6c4f355a5a704180c1982f98d5ffa6b3923ba373
Hercules Hamanaka <hercules.hamanaka@gmail.com>
2021-09-24 23:53:30 -0300

    feat: Dashboard user info added

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
-import { Container, Title } from './styles';
+
+
+import { 
+  Container, 
+  Header,
+  UserInfoContainer,
+  UserInfo,
+  UserAvatar,
+  User,
+  UserGreeting,
+  UserName,
+  Icon
+ } from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserInfoContainer>
+          <UserInfo>
+            <UserAvatar source={{ uri: 'https://avatars.githubusercontent.com/u/33010639?s=40&v=4' }} />
+            <User>
+              <UserGreeting>Hi,</UserGreeting>
+              <UserName>Hercules.</UserName>
+            </User>
+          </UserInfo>       
+          <Icon name="power" />
+        </UserInfoContainer>        
+      </Header>
     </Container>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,14 +1,73 @@
 import styled from 'styled-components/native';
+import { Feather } from '@expo/vector-icons';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+import theme from '../../global/styles/theme';
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: #9199;
+  /** background-color: ${( props ) => props.theme.colors.primary }; */
+  background-color: ${({ theme }) => theme.colors.background };
 `;
 
+/**
 export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.bold };
   font-size: 24px;
   font-weight: bold;
-  color: #000;
+  color: ${({ theme }) => theme.colors.title};
+`;**/
+
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFPercentage(42)}px;
+  background-color: ${({ theme }) => theme.colors.primary};
+
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const UserInfoContainer = styled.View`
+  width: 100%;
+  padding: 0 24px;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const UserInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const UserAvatar = styled.Image`
+  width: ${RFValue(50)}px;
+  height: ${RFValue(50)}px;
+  border-radius: 10px;
+`;
+
+export const User = styled.View`
+  margin-left: 17px;
+`;
+
+export const UserGreeting = styled.Text`
+  color: ${({ theme}) => theme.colors.shape};
+  
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+
+`;
+
+export const UserName = styled.Text`
+  color: ${({ theme}) => theme.colors.shape};
+  
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.bold};
+
+`;
+
+export const Icon = styled(Feather)`
+  color: ${({ theme }) => theme.colors.secondary};
+  font-size: ${RFValue(24)}px;
 `;
